ff5f02592a694d7c0555e3ecac38c68340e75588
Florian Schäfer <florian@schaeferban.de>
2018-11-02 20:51:58 +0100

    Convert *.lang files to *.po, move Java classes to the JOSM plugins package

---
+++ src/org/openstreetmap/josm/plugins/ptl/Plugin.java
@@ -0,0 +1,18 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.ptl;
+
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * Public transport layer plugin.
+ */
+public class Plugin extends org.openstreetmap.josm.plugins.Plugin {
+
+    public Plugin(PluginInformation info) {
+        super(info);
+        MainMenu.add(MainApplication.getMenu().dataMenu, new PublicTransportLayer.AddLayerAction());
+        MainMenu.add(MainApplication.getMenu().dataMenu, new DistanceBetweenStops());
+    }
+}
