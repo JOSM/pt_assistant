1cf6ecb42b61ee829b5270005402403d81a48699
Sudhanshu Agarwal <24910339+sudhanshu2@users.noreply.github.com>
2020-06-01 23:58:33 +0530

    Inititialized refactoring by creating new classes and package
    Added mend_action_relation package, AbstractMendRelation, DownloadArea, PersonalTransportMendRelation and PublicTransportMendRelation

---
+++ src/main/java/org/openstreetmap/josm/plugins/pt_assistant/actions/mend_relation_action/AbstractMendRelationAction.java
@@ -0,0 +1,5 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.pt_assistant.actions.mend_relation_action;
+
+public class AbstractMendRelationAction {
+}
---
+++ src/main/java/org/openstreetmap/josm/plugins/pt_assistant/actions/mend_relation_action/DownloadArea.java
@@ -0,0 +1,7 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.pt_assistant.actions.mend_relation_action;
+
+public class DownloadArea {
+
+
+}
